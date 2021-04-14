0e5f6e838f2e844f4cd434d329c98a5687ddf331
lionelritchie29 <lionelritchie29@github.com>
2021-04-14 16:17:20 +0700

    added query params for get all books

--- src/handlers/get-all-books-handler.js
+++ src/handlers/get-all-books-handler.js
@@ -4,13 +4,32 @@ const {
 } = require('../handler-utils/response-creator');
 
 const getAllBooksHandler = (request, h) => {
-  const filteredBooks = getFilteredBooks();
+  const {name, reading, finished} = request.query;
+  let filteredBooks = null;
+
+  if (name) {
+    const booksByName = books.filter((book) => {
+      return book.name.toUpperCase().includes(name.toUpperCase());
+    });
+    filteredBooks = getFilteredBooks(booksByName);
+  } else if (reading) {
+    const readBool = reading === '1' ? true : false;
+    const booksByReading = books.filter((book) => book.reading === readBool);
+    filteredBooks = getFilteredBooks(booksByReading);
+  } else if (finished) {
+    const finishBool = finished === '1' ? true : false;
+    const booksByFinished =
+      books.filter((book) => book.finished === finishBool);
+    filteredBooks = getFilteredBooks(booksByFinished);
+  } else {
+    filteredBooks = getFilteredBooks(books);
+  }
   return getSuccessResponseWithData(h, {books: filteredBooks});
 };
 
-const getFilteredBooks = () => {
+const getFilteredBooks = (unfilteredBooks) => {
   const filteredBooks = [];
-  books.forEach((book) => {
+  unfilteredBooks.forEach((book) => {
     const {id, name, publisher} = book;
     filteredBooks.push({id, name, publisher});
   });
